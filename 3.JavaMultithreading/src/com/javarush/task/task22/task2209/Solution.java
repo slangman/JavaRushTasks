73b20d9ba50f0258c0f2e1720e595f3ef4ec5731
slangman <mc.slangman@gmail.com>
2018-02-22 13:31:42 +0600

    Addded new solved tasks: 2210 2211
    Task 2212 solved partially.
    Issue with brackets condition.

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2209/Solution.java
@@ -4,9 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /*
 Составить цепочку слов
